7a871e29661e79968deee88f6c69e70720483fdf
stupidcoderathere <franklinwong207@gmail.com>
2026-07-23 22:21:48 +0800

    Reunify the forked ratata crate

--- src-tauri/crates/omega-cli/ratata_src/screen.rs
+++
@@ -1,9 +0,0 @@
-use ratatui::Frame;
-
-use crate::{message::Message, command::Command};
-
-pub trait Screen {
-    fn render(&self, f: &mut Frame<'_>);
-
-    fn update(&mut self, message: Message) -> Option<Command>;
-}
